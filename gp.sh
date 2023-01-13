80cbcdf98ab7cce1df36a2e469ebdda636244eaa
mort1skoda <mort1skoda@gmail.com>
2023-01-13 18:08:41 +0100

    2023-01-13 18:08:41 Friday

--- gp.sh
+++ gp.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-git init
+git remote -v
+git status
 git add --all
+git status
 DATE=$(date +'%Y-%m-%d %H:%M:%S %aday')
 git commit -m "$DATE"
 git branch -M master
+#git remote add origin https://github.com/mort1skoda/devLfs.git
 git push -u origin master
+
+#TODO:
+#git pull
+
 
